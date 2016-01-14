f6e86410d8a76f01777cee8af213f161d60868c6
greggreg <greggreg@itsgreggreg.com>
2016-01-14 16:09:54 -0600

    corrected naming

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Romanex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :roman_numerals,
+    [app: :romanex,
      version: "0.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
